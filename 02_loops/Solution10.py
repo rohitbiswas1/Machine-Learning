11360a6db14a01f388bd703fc95999a0a202eaaa
rohitbiswas1 <108587295+rohitbiswas1@users.noreply.github.com>
2024-09-12 02:34:13 +0000

    Made some changes

--- 02_loops/Solution10.py
+++ 02_loops/Solution10.py
@@ -5,4 +5,8 @@ wait_time = 2
 max_retries = 5
 attempts = 0
 
-while attempts 
+while attempts < max_retries:
+    print("Attempt",attempts+1, " - Wait time", wait_time)
+    time.sleep(wait_time)
+    wait_time*= 2
+    attempts+=1
